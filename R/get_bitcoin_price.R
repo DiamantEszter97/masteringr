946c2f02678ae765ea282812f0ab93f04885049c
Diamant Eszter <diamanteszter97@gmail.com>
2021-05-18 21:56:24 +0200

    get_bitcoin_price is corrected, get_exchange_rate created and added

--- R/get_bitcoin_price.R
+++ R/get_bitcoin_price.R
@@ -1,11 +1,14 @@
 #' get HUf price of given number of BTC
+#' @param x number
 #' @param retried number
 #' @return number
 #' @inheritParams get_usdhuf
 #' @inheritParams forint
 #' @export
-#' @importFrom logger log_info
-#'  @importFrom binancer binance_coins_prices
+#' @importFrom checkmate assert_number
+#' @importFrom jsonlite fromJSON
+#' @importFrom logger log_error log_info
+#' @importFrom binancer binance_coins_prices
 #' @example
 #' get_bitcoin_price(1)
 
@@ -14,17 +17,29 @@ library(binancer)
 library(logger)
 
 
-get_bitcoin_price<- function(x) {
+get_bitcoin_price<- function(x, retried = 0) {
     BITCOINS <- x
+    tryCatch({
+
     log_info('Number of Bitcoins: {BITCOINS}')
 
     usdhuf <- get_usdhuf()
+
+    assert_number(usdhuf, lower = 250, upper = 400)
 
     btcusd <- binance_coins_prices()[symbol == 'BTC', usd]
     log_info('1 BTC={dollar(btcusd)}')
 
     log_info('My crypto fortune is {forint(BITCOINS * btcusd * usdhuf)}')
 
-    return(BITCOINS * btcusd * usdhuf)
 
+
+
+    }, error = function(e) {
+        log_error(e$message)
+        Sys.sleep(1 + retried^2)
+        get_bitcoin_price(retried = retried + 1)
+
+    })
+    return(BITCOINS * btcusd * usdhuf)
 }
